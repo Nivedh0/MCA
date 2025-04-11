07c30c14943048a7ef10427221987ed81512ce87
aswin3417 <aswinac090@gmail.com>
2025-04-11 14:43:38 +0530

    Update Exercise_1.sql

--- S2_Lab/ADBMS/Exercise_1.sql
+++ S2_Lab/ADBMS/Exercise_1.sql
@@ -1,9 +1,9 @@
 1.Create a database;
-    create database employee;
+mysql>create database employee;
     Query OK, 1 row affected (0.01 sec)
       
 2.Create table Employee with the following  filed – empno int primary key,ename varchar(30),JOB varchar(10),managerid int,hiredate date,salary int,commision int,deptno int
-     CREATE TABLE Employee (
+mysql> CREATE TABLE Employee (
     ->     empno INT PRIMARY KEY,
     ->     ename VARCHAR(30),
     ->     JOB VARCHAR(10),
@@ -16,7 +16,7 @@
 Query OK, 0 rows affected (0.04 sec)
   
 3.describe schema.
-     DESCRIBE Employee;
+mysql>DESCRIBE Employee;
 +-----------+-------------+------+-----+---------+-------+
 | Field     | Type        | Null | Key | Default | Extra |
 +-----------+-------------+------+-----+---------+-------+
@@ -32,7 +32,7 @@ Query OK, 0 rows affected (0.04 sec)
 8 rows in set (0.00 sec)
 
 4.create a table department with the following field -deptno int primary key,deptname varchar(15),location varchar(10)
-     CREATE TABLE Department (
+mysql>CREATE TABLE Department (
     ->     deptno INT PRIMARY KEY,
     ->     deptname VARCHAR(15),
     ->     location VARCHAR(10)
@@ -40,7 +40,7 @@ Query OK, 0 rows affected (0.04 sec)
   Query OK, 0 rows affected (0.02 sec)
   
 5.describe schema
-    DESCRIBE Department;
+mysql>DESCRIBE Department;
 +----------+-------------+------+-----+---------+-------+
 | Field    | Type        | Null | Key | Default | Extra |
 +----------+-------------+------+-----+---------+-------+
@@ -51,7 +51,7 @@ Query OK, 0 rows affected (0.04 sec)
 3 rows in set (0.00 sec)
   
 6. create a table salarygrade with the following field - (grade int primary key,lowsalary int,highsalary int);
-     CREATE TABLE SalaryGrade (
+mysql>CREATE TABLE SalaryGrade (
     ->     grade INT PRIMARY KEY,
     ->     lowsalary INT,
     ->     highsalary INT
@@ -59,7 +59,7 @@ Query OK, 0 rows affected (0.04 sec)
 Query OK, 0 rows affected (0.02 sec)
   
 7.describe schema
-   DESCRIBE SalaryGrade;
+mysql>DESCRIBE SalaryGrade;
 +------------+------+------+-----+---------+-------+
 | Field      | Type | Null | Key | Default | Extra |
 +------------+------+------+-----+---------+-------+
