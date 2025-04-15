1334364449f84f138ced1d0fb720ac6927da9082
Nivedh0 <nived385@gmail.com>
2025-04-15 12:09:25 +0530

    Update multiplication_table.sh

--- S2_Lab/CN/multiplication_table.sh
+++ S2_Lab/CN/multiplication_table.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
-echo "Enter the upper limit for the multiplication tables:"
-read limit
-echo "Multiplication table for $limit:"
-for (( j=1; j<=10; j++ ))
-do
-	echo "$limit x $j = $(( limit * j ))"
+echo "Enter a number to print its multiplication table: "
+read num
+i=1
+while [ $i -le 10 ]; do
+    echo "$i * $num = $((num * i))"
+    ((i++))
 done
 
 
