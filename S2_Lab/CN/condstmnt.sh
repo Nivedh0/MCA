9b125f285c0cb253bed776691468a22e43f4a7e9
Nivedh0 <nived385@gmail.com>
2025-04-15 11:37:47 +0530

    Add files via upload

---
+++ S2_Lab/CN/condstmnt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "enter 3 no."
+read a b c
+
+if [[ $a -gt $b && $a -gt $c ]]
+then
+	echo "$a is greater"
+
+elif [[ $b -gt $a && $b -gt $c ]]
+then
+	echo "$b is greater"
+
+else
+	echo "$c is greater"
+fi
+
+
+
